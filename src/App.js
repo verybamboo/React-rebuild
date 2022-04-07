e395be022e26350aeb2c8e9657814f613a7802c7
George Chen <georgechen1338@gmail.com>
2022-04-07 17:35:13 -0400

    finally prints

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
+  const [characters, setCharacters] = useState(false);
+  let index = 0;
+
+  useEffect(() => {
+    fetch("https://www.moogleapi.com/api/v1/characters")
+      .then((response) => response.json())
+      .then((result) => setCharacters(result));
+  }, []);
+
+  if (characters === undefined) {
+    return <>Still loading...</>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainbox">
+      <div className="leftSide">
+        {characters && <h1>Name: {characters[index].name}</h1>}
+        {characters && <p>Description: {characters[index].description}</p>}
+      </div>
+      <div class="slides">
+        <div className="images">
+          {characters && (
+            <img src={characters[index].pictures[0].url} alt="charImage" />
+          )}
+        </div>
+      </div>
+      <div class="rightSide">
+        {characters && <h1>Japanese Name: {characters[index].japaneseName}</h1>}
+        {characters && <p>Age: {characters[index].age}</p>}
+        {characters && <p>Gender: ${characters[index].gender}</p>}
+        {characters && <p>Height: ${characters[index].height}</p>}
+        {characters && <p>Job: ${characters[index].job}</p>}
+        {characters && <p>Origin: ${characters[index].origin}</p>}
+        {characters && <p>Race: ${characters[index].race}</p>}
+        {characters && <p>Weight: ${characters[index].weight}</p>}
+      </div>
     </div>
   );
 }
