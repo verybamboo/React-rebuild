8ba6b09fce65ff141ad3ab54b7a67ff528de58a6
George Chen <georgechen1338@gmail.com>
2022-04-11 12:20:03 -0400

    cleaned up code

--- src/Image.jsx
+++ src/Image.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import missingPic from "./img/imageavailable.png"
 
 function Image(props) {
-  // const theImage = props.characters && props.characters[props.index].pictures[0]
-  //   ? props.characters[props.index].pictures[0]
-  //   : missingPic
+  const theImage = props.characters && props.characters[props.index].pictures[0]
+    ? props.characters[props.index].pictures[0].url
+    : missingPic
 
   return (
     <div className="slides">
     {props.characters && (
-      <img className="charImage" src={props.characters && props.characters[props.index].pictures[0]
-        ? props.characters[props.index].pictures[0].url
-        : missingPic} alt="charImage" />
+      <img className="charImage" src={theImage} alt="charImage" />
     )}
     </div>
   )
